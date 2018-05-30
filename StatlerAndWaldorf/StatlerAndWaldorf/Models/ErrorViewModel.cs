5e157676bbda288a74e864e9509e042c458c2ed4
Dor Sivandddג <dorsiv@gmail.comd>
2018-05-30 20:43:44 +0300

    do

---
+++ StatlerAndWaldorf/StatlerAndWaldorf/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace StatlerAndWaldorf.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
